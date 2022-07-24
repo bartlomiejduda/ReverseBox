647eb3cf476da2b1314a71c5419ecbca6595a10c
Bartlomiej Duda <ikskoks@gmail.com>
2022-07-25 00:50:00 +0200

    Added more CRC32 tests

--- tests/tests_checksum/test_checksum_crc32_iso_hdlc.py
+++ tests/tests_checksum/test_checksum_crc32_iso_hdlc.py
@@ -8,19 +8,60 @@ import pytest
 from faker import Faker
 from reversebox.checksum.checksum_crc32_iso_hdlc import CRC32Handler
 from reversebox.common.common import convert_int_to_hex_string
+from tests.common import CRCTestEntry
 
 crc32_handler = CRC32Handler()
 fake = Faker()
 
 
 @pytest.mark.unittest
-def test_checksum_calculate_crc32_to_match_zlib_result():
+def test_checksum_calculate_crc32_iso_hdlc_to_match_expected_result():
+    crc_data_list = [
+        CRCTestEntry(
+            test_data=b"123456789", expected_int=3421780262, expected_str="0xCBF43926"
+        ),
+        CRCTestEntry(
+            test_data=b"123", expected_int=2286445522, expected_str="0x884863D2"
+        ),
+        CRCTestEntry(test_data=b"", expected_int=0, expected_str="0x00"),
+        CRCTestEntry(
+            test_data=b" ", expected_int=3916222277, expected_str="0xE96CCF45"
+        ),
+        CRCTestEntry(
+            test_data=b"\xAA\xBB", expected_int=1233267864, expected_str="0x49822C98"
+        ),
+        CRCTestEntry(
+            test_data=b"\x01\x02\x03\x04",
+            expected_int=3057449933,
+            expected_str="0xB63CFBCD",
+        ),
+    ]
+
+    for crc_entry in crc_data_list:
+        # check expected result from first execution
+        test_result = crc32_handler.calculate_crc32(crc_entry.test_data)
+        test_result_str = convert_int_to_hex_string(test_result)
+        assert test_result == crc_entry.expected_int
+        assert test_result_str == crc_entry.expected_str
+
+        # check if result is the same after second execution
+        test_result = crc32_handler.calculate_crc32(crc_entry.test_data)
+        test_result_str = convert_int_to_hex_string(test_result)
+        assert test_result == crc_entry.expected_int
+        assert test_result_str == crc_entry.expected_str
+
+
+@pytest.mark.unittest
+def test_checksum_calculate_crc32_iso_hdlc_to_match_zlib_result():
 
     test_data_array = [
         b"",
+        b" ",
         b"123456789",
+        b"123",
         b"Hello Python",
         b"Secret123@123#!",
+        b"\xAA\xBB",
         b"\x00\x01\x02\x03",
         b"1",
         b"abcdefgh",
@@ -37,22 +78,3 @@ def test_checksum_calculate_crc32_to_match_zlib_result():
         zlib_result_str = convert_int_to_hex_string(zlib_result)
         assert test_result == zlib_result
         assert test_result_str == zlib_result_str
-
-
-@pytest.mark.unittest
-def test_checksum_calculate_crc32_to_match_expected_result():
-    test_data = b"123456789"
-    expected_result = 3421780262
-    expected_result_str = "0xCBF43926"
-
-    # check expected result from first execution
-    test_result = crc32_handler.calculate_crc32(test_data)
-    test_result_str = convert_int_to_hex_string(test_result)
-    assert test_result == expected_result
-    assert test_result_str == expected_result_str
-
-    # check if result is the same after second execution
-    test_result = crc32_handler.calculate_crc32(test_data)
-    test_result_str = convert_int_to_hex_string(test_result)
-    assert test_result == expected_result
-    assert test_result_str == expected_result_str
