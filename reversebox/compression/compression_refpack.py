546f24aad303c8860d0ad9b28d8650b69ef11675
Bartlomiej Duda <ikskoks@gmail.com>
2024-05-06 18:05:19 +0200

    v0.9.5

--- reversebox/compression/compression_refpack.py
+++ reversebox/compression/compression_refpack.py
@@ -11,10 +11,36 @@ class RefpackHandler:
     def compress_data(self, input_data: bytes) -> bytes:
         return input_data  # TODO - fix this
 
+    def _get_output_buffer_size(self, compressed_data: bytes) -> int:
+        current_input_offset: int = 0
+        compression_type = compressed_data[current_input_offset]
+        current_input_offset += 1
+        compression_type = (compression_type << 8) + compressed_data[
+            current_input_offset
+        ]
+        current_input_offset += 1
+
+        if compression_type & 0x100:
+            current_input_offset += 3  # skip uncompressed_size
+
+        uncompressed_size: int = compressed_data[current_input_offset]
+        current_input_offset += 1
+        uncompressed_size = (uncompressed_size << 8) + compressed_data[
+            current_input_offset
+        ]
+        current_input_offset += 1
+        uncompressed_size = (uncompressed_size << 8) + compressed_data[
+            current_input_offset
+        ]
+        current_input_offset += 1
+        return uncompressed_size
+
     def decompress_data(self, compressed_data: bytes) -> bytes:
+        if len(compressed_data) < 5:
+            raise Exception("Compressed data too short!")
         if compressed_data[:2] != b"\x10\xFB":
             raise Exception("Wrong refpack compression header!")
-        temp_buffer = (c_char * len(compressed_data) * 100)()
+        temp_buffer = (c_char * self._get_output_buffer_size(compressed_data))()
         try:
             refpack_dll_path: str = str(
                 Path(__file__)
