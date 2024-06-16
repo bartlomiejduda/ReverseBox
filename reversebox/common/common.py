0cab332c3ffce9c184144c7c37e4a94217630fc6
Bartlomiej Duda <ikskoks@gmail.com>
2024-06-16 22:42:12 +0200

    v0.11.8

--- reversebox/common/common.py
+++ reversebox/common/common.py
@@ -1,11 +1,11 @@
 """
-Copyright © 2022  Bartłomiej Duda
+Copyright © 2022-2024  Bartłomiej Duda
 License: GPL-3.0 License
 """
 
 
-def convert_int_to_hex_string(input_checksum: int) -> str:
-    return "0x%02X" % int(input_checksum)
+def convert_int_to_hex_string(input_value: int) -> str:
+    return "0x%02X" % int(input_value)
 
 
 def convert_hex_string_to_int(input_string: str) -> int:
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import os
 
 import setuptools
 
-VERSION_NUM = "0.11.7"
+VERSION_NUM = "0.11.8"
 
 
 def get_long_description() -> str:
