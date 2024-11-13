c6f5648d3717f69baae87ca75bb3816b799540d9
Bartlomiej Duda <ikskoks@gmail.com>
2024-11-13 21:46:22 +0100

    v0.18.4

--- reversebox/compression/compression_refpack.py
+++ reversebox/compression/compression_refpack.py
@@ -3,22 +3,40 @@ Copyright © 2024  Bartłomiej Duda
 License: GPL-3.0 License
 """
 import ctypes
-from ctypes import byref, c_char
+from ctypes import c_char
 
 from reversebox.common.common import get_dll_path
 
+# fmt: off
+
 
 class RefpackHandler:
-    def compress_data(self, input_data: bytes) -> bytes:
-        return input_data  # TODO - fix this
+    def compress_data(self, uncompressed_data: bytes) -> bytes:
+        if len(uncompressed_data) == 0:
+            raise Exception("Data is empty! Nothing to compress!")
+
+        maxback: int = 131072
+        quick: int = 0
+
+        c_uncompressed_data_len = ctypes.c_uint(len(uncompressed_data))
+        c_uncompressed_data = (ctypes.c_ubyte * len(uncompressed_data))(*uncompressed_data)
+        c_compressed_data_buffer = (c_char * int(len(uncompressed_data)*5))()  # just in case
+        c_options_maxback = ctypes.c_uint(maxback)
+        c_options_quick = ctypes.c_uint(quick)
+
+        try:
+            refpack_dll_path: str = get_dll_path("refpack.dll")
+            refpack_dll_file = ctypes.CDLL(refpack_dll_path)
+            compressed_data_size = refpack_dll_file.refpack_compress(ctypes.byref(c_compressed_data_buffer), c_uncompressed_data, c_uncompressed_data_len, ctypes.byref(c_options_maxback), ctypes.byref(c_options_quick))
+        except Exception as error:
+            raise Exception(f"Error while decompressing refpack data! Error: {error}")
+        return bytes(bytearray(c_compressed_data_buffer)[:compressed_data_size])
 
     def _get_output_buffer_size(self, compressed_data: bytes) -> int:
         current_input_offset: int = 0
         compression_type = compressed_data[current_input_offset]
         current_input_offset += 1
-        compression_type = (compression_type << 8) + compressed_data[
-            current_input_offset
-        ]
+        compression_type = (compression_type << 8) + compressed_data[current_input_offset]
         current_input_offset += 1
 
         if compression_type & 0x100:
@@ -26,13 +44,9 @@ class RefpackHandler:
 
         uncompressed_size: int = compressed_data[current_input_offset]
         current_input_offset += 1
-        uncompressed_size = (uncompressed_size << 8) + compressed_data[
-            current_input_offset
-        ]
+        uncompressed_size = (uncompressed_size << 8) + compressed_data[current_input_offset]
         current_input_offset += 1
-        uncompressed_size = (uncompressed_size << 8) + compressed_data[
-            current_input_offset
-        ]
+        uncompressed_size = (uncompressed_size << 8) + compressed_data[current_input_offset]
         current_input_offset += 1
         return uncompressed_size
 
@@ -41,15 +55,14 @@ class RefpackHandler:
             raise Exception("Compressed data too short!")
         if compressed_data[:2] != b"\x10\xFB":
             raise Exception("Wrong refpack compression header!")
-        uncompressed_data_buffer = (
-            c_char * self._get_output_buffer_size(compressed_data)
-        )()
+
+        uncompressed_data_buffer = (c_char * self._get_output_buffer_size(compressed_data))()
+        compressed_data_len = ctypes.c_uint(len(compressed_data))
+
         try:
             refpack_dll_path: str = get_dll_path("refpack.dll")
             refpack_dll_file = ctypes.CDLL(refpack_dll_path)
-            uncompressed_data_size = refpack_dll_file.unrefpack(
-                compressed_data, byref(uncompressed_data_buffer), 1
-            )
+            uncompressed_data_size = refpack_dll_file.refpack_decompress(ctypes.byref(uncompressed_data_buffer), compressed_data, ctypes.byref(compressed_data_len))
         except Exception as error:
             raise Exception(f"Error while decompressing refpack data! Error: {error}")
         return bytes(bytearray(uncompressed_data_buffer)[:uncompressed_data_size])
--- tests/tests_compression/test_compression_refpack.py
+++ tests/tests_compression/test_compression_refpack.py
@@ -8,23 +8,29 @@ import pytest
 
 from reversebox.compression.compression_refpack import RefpackHandler
 
-# TODO - uncomment this when compressor will be implemented
-# @pytest.mark.unittest
-# def test_open_and_compress_file_with_refpack():
-#     uncompressed_file_path = os.path.join(
-#         os.path.dirname(__file__), "compression_data\\fake_file03_refpack_uncompressed.bin"
-#     )
-#     compressed_file_path = os.path.join(
-#         os.path.dirname(__file__), "compression_data\\fake_file03_refpack_compressed.bin"
-#     )
-#
-#     uncompressed_input_file_data = open(uncompressed_file_path, "rb").read()
-#     compressed_input_file_data = open(compressed_file_path, "rb").read()
-#
-#     refpack_handler = RefpackHandler()
-#     compressed_data = refpack_handler.compress_data(uncompressed_input_file_data)
-#
-#     assert compressed_data == compressed_input_file_data
+
+@pytest.mark.unittest
+def test_open_and_compress_file_with_refpack():
+    uncompressed_file_path = os.path.join(
+        os.path.dirname(__file__),
+        "compression_data\\fake_file03_refpack_uncompressed.bin",
+    )
+    compressed_file_path = os.path.join(
+        os.path.dirname(__file__),
+        "compression_data\\fake_file03_refpack_compressed.bin",
+    )
+
+    uncompressed_input_file_data = open(uncompressed_file_path, "rb").read()
+    compressed_input_file_data = open(compressed_file_path, "rb").read()
+
+    refpack_handler = RefpackHandler()
+    compressed_data = refpack_handler.compress_data(uncompressed_input_file_data)
+
+    assert len(compressed_data) <= len(compressed_input_file_data) + 100
+    assert len(compressed_data) > 0
+    assert (
+        compressed_data[:2] == compressed_input_file_data[:2]
+    )  # check for 10FB signature
 
 
 @pytest.mark.unittest
@@ -45,3 +51,5 @@ def test_open_and_decompress_file_with_refpack():
     uncompressed_data = refpack_handler.decompress_data(compressed_input_file_data)
     assert len(uncompressed_data) == len(uncompressed_input_file_data)
     assert type(uncompressed_data) == type(uncompressed_input_file_data)
+    assert uncompressed_data[:10] == uncompressed_input_file_data[:10]
+    assert uncompressed_data[-10:] == uncompressed_input_file_data[-10:]
