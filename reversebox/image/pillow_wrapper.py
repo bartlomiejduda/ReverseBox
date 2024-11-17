d774b2bff1d4db047cfe9312ad588d32055f7d10
Bartlomiej Duda <ikskoks@gmail.com>
2024-11-17 22:40:28 +0100

    v0.18.7

--- reversebox/image/pillow_wrapper.py
+++ reversebox/image/pillow_wrapper.py
@@ -3,6 +3,8 @@ Copyright © 2024  Bartłomiej Duda
 License: GPL-3.0 License
 """
 
+import io
+
 from PIL import Image
 
 
@@ -44,3 +46,27 @@ class PillowWrapper:
 
     def get_image_data_from_pillow_image(self, pillow_image: Image) -> bytes:
         return pillow_image.tobytes()
+
+    def get_pil_image_file_data_for_export(
+        self, image_data: bytes, img_width: int, img_height, pillow_format: str = "DDS"
+    ) -> bytes:
+        pil_image: Image = Image.frombuffer(
+            "RGBA",
+            (int(img_width), int(img_height)),
+            image_data,
+            "raw",
+            "RGBA",
+            0,
+            1,
+        )
+        image_buffer = io.BytesIO()
+        pil_image.save(image_buffer, pillow_format)
+        image_buffer.seek(0)
+        pil_image_file_data: bytes = image_buffer.read()
+        return pil_image_file_data
+
+    def get_pil_rgba_data_for_import(self, file_path: str) -> bytes:
+        pil_image: Image = Image.open(file_path)
+        pil_image = pil_image.convert("RGBA")
+        rgba_data: bytes = pil_image.tobytes()
+        return rgba_data
