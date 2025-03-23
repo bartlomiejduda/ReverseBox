d39b7fd26e046b49d7c10b0de25865ca4ce7ab3d
Bartlomiej Duda <ikskoks@gmail.com>
2025-03-23 17:32:48 +0100

    add font dmg0 test

--- tests/tests_image/test_swizzle_ps_vita.py
+++ tests/tests_image/test_swizzle_ps_vita.py
@@ -128,3 +128,40 @@ def test_ps_vita_unswizzle_and_swizzle_monkey_dxt5():
     assert swizzled_file_data[1000:1100] == reswizzled_file_data[1000:1100]
     assert swizzled_file_data[3000:3100] == reswizzled_file_data[3000:3100]
     # assert swizzled_file_data[-100:] == reswizzled_file_data[-100:]  # TODO - why it's not working?
+
+
+@pytest.mark.imagetest
+def test_ps_vita_unswizzle_and_swizzle_font_dmg0():
+    swizzled_file_path = os.path.join(
+        os.path.dirname(__file__), "image_files/swizzle_ps_vita_font_dmg0.bin"
+    )
+
+    bin_file = open(swizzled_file_path, "rb")
+    swizzled_file_data = bin_file.read()
+
+    img_width = 512
+    img_height = 32
+    bpp = 8
+    image_format = ImageFormats.BC3_DXT5
+
+    unswizzled_file_data = unswizzle_psvita_dreamcast(swizzled_file_data, img_width, img_height, bpp, block_width_height=4)
+
+    # debug start ###############################################################################################
+    is_debug = False
+    if is_debug:
+        image_decoder = ImageDecoder()
+        wrapper = PillowWrapper()
+        decoded_image_data: bytes = image_decoder.decode_compressed_image(
+            unswizzled_file_data, img_width, img_height, image_format
+        )
+        pil_image = wrapper.get_pillow_image_from_rgba8888_data(decoded_image_data, img_width, img_height)
+        pil_image.show()
+    # debug end #################################################################################################
+
+    reswizzled_file_data = swizzle_psvita_dreamcast(unswizzled_file_data, img_width, img_height, bpp, block_width_height=4)
+
+    assert len(swizzled_file_data) == len(reswizzled_file_data)
+    assert swizzled_file_data[:100] == reswizzled_file_data[:100]
+    assert swizzled_file_data[1000:1100] == reswizzled_file_data[1000:1100]
+    assert swizzled_file_data[3000:3100] == reswizzled_file_data[3000:3100]
+    assert swizzled_file_data[-100:] == reswizzled_file_data[-100:]
